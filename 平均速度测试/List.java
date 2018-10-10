a961ae5f53cd8898d1134f9fa58e9f1cfa1ebe90
Iamsleepingkeepaway <43072501+Iamsleepingkeepaway@users.noreply.github.com>
2018-10-10 20:03:13 +0800

    Add files via upload

---
+++ 平均速度测试/List.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.ArrayList;
+public class List {
+public static void main(String args[]) {
+	int n=10000;
+	long time=0;
+	ArrayList<Integer> list=new ArrayList<>();
+	for(int i=0;i<n;i++) {
+		list.add(i);
+				}
+	
+	
+	for(int i=0;i<n;i++)
+	{
+		int j=list.get(i);
+		long start=System.nanoTime();
+		boolean k=list.contains(j);
+		long end=System.nanoTime();
+		long t=end-start;
+		time=time+t;
+	}
+	
+	System.out.println("平均查询速度为："+time/n+"ns");
+}
+}
